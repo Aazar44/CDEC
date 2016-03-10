cacff52101a81356188c1abebe104aa8a60d79f3
Aazar44 <Aazar44@users.noreply.github.com>
2016-03-10 18:29:22 +0500

    Added files via upload

---
+++ Notaclock.go
@@ -0,0 +1,11 @@
+//Package notaclock does not tell time
+package notaclock
+
+//Doesnottelltime is a global variable
+var Doesnottelltime = "This application does not tell time."
+
+//Donottelltime is a global function
+func Donottelltime() string 
+{
+	return Doesnottelltime
+}
